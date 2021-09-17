d8c1663c71692f62946c474ee9b6b19f4d18ebd8
Aruming <0430kar@naver.com>
2021-09-17 14:02:54 +0900

    210917 백준 단계별 입출력과 사칙연산

---
+++ src/lv1/No1008.java
@@ -0,0 +1,15 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class No1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		System.out.println(a/b);
+	}
+
+}
---
+++ src/lv1/No10998.java
@@ -0,0 +1,15 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class No10998 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a*b);
+	}
+
+}
