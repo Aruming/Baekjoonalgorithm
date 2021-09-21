0c4309cf810a10d06397314eda80a54f8c267914
Aruming <0430kar@naver.com>
2021-09-21 14:16:21 +0900

    210921 백준 단계별 for문 15552(try-catch)

---
+++ src/lv3/No15552_2.java
@@ -0,0 +1,32 @@
+package lv3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No15552_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int t = Integer.parseInt(br.readLine());
+			
+			for(int i=0;i<t;i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine()," ");
+				bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+			}
+			br.close();
+			bw.flush();
+			bw.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
