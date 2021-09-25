4c568d3f4d3538d5e27ee9529eae759e46c6ab7c
Aruming <0430kar@naver.com>
2021-09-25 17:10:59 +0900

    210925 백준 단계별 기본수학1

--- src/lv8/No1712.java
+++ src/lv8/No1712.java
@@ -10,16 +10,16 @@ public class No1712 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String num[] = br.readLine().split(" ");
-		int a = Integer.parseInt(num[0]);
+		String num[] = br.readLine().split(" ");  //공백을 기준으로 나누어 배열 저장
+		int a = Integer.parseInt(num[0]);  //문자 정수화
 		int b = Integer.parseInt(num[1]);
 		int c = Integer.parseInt(num[2]);
 		
-		if(b>c) {
+		if(b>=c) {  //손익분기점 없을 때
 			System.out.println("-1");
 		}else {
 			int n = a/(c-b);
-			System.out.println(n);
+			System.out.println(n+1);
 		}
 		
 	}
