976beef69c2621ca2f87dcaca7a2a6ddb8a90d7e
Aruming <0430kar@naver.com>
2021-09-28 20:27:56 +0900

    210928 백준 단계별 기본수학1

--- src/lv8/No2869.java
+++ src/lv8/No2869.java
@@ -1,24 +1,27 @@
 package lv8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class No2869 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a, b, v;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		v = sc.nextInt();
+		String[] st = br.readLine().split(" ");
+		a = Integer.parseInt(st[0]);
+		b = Integer.parseInt(st[1]);
+		v = Integer.parseInt(st[2]);
 		
-		int day = 0;
-		while(v>0){
-			day++;
-			v -= a;
-			if(v==0) break;
-			else v += b;
-			
+		int day=0;
+		if((v-a)%(a-b)==0) {
+			day = (v-a)/(a-b) + 1;
+		}
+		else {
+			day = (v-a)/(a-b) + 2 ;
 		}
 		
 		System.out.println(day);
