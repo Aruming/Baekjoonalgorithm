391d7ffdcd3a7f8f8a6bb78922b9aa52959ac0d2
Aruming <0430kar@naver.com>
2021-10-10 17:02:27 +0900

    211010 백준 단계별 기본수학1
    sqrt() 메소드 사용하여 제곱근 구한 뒤 경우에 따라 cnt 값 정의

--- src/lv8/No1011.java
+++ src/lv8/No1011.java
@@ -8,43 +8,26 @@ public class No1011 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		int x, y, distance, cnt, move;
+		int x, y, distance, cnt = 0, max;
 		
 		for(int i=0;i<t;i++) {
 			x = sc.nextInt();
 			y = sc.nextInt();
-			distance = y-x;
-			move = 1;
 			
-			if(distance==1) {
-				cnt = 1;
-			}
-			else if(distance==2) {
-				cnt = 2;
-			}
-			else {
-				cnt=2;
-				distance -=2;
-				while(distance>0) {
-					if(distance>move) {
-						distance-=(move+1);
-						move = move+1;
-						cnt++;
-					}
-					else if(distance == move) {
-						cnt++;
-						break;
-					}
-					else {
-						distance -= (move-1);
-						move = move-1;
-						cnt++;
-					}
-				}
-			}
+			distance = y-x;
+			max = (int)Math.sqrt(distance);
+			
+			if(max*max==distance)
+				cnt = 2*max - 1;
+			else if(distance<=(max*max)+max)
+				cnt = 2*max;
+			else
+				cnt = 2*max + 1;
 			
 			System.out.println(cnt);
 		}
+		
 	}
-
 }
+
+
