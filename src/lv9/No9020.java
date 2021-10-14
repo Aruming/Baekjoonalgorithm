c9fa65c48c623de0277b4b5eb7d92238ab83d926
Aruming <0430kar@naver.com>
2021-10-14 19:52:20 +0900

    211014 백준 단계별 기본수학2
    #9020 골드바흐의 추측

---
+++ src/lv9/No9020.java
@@ -0,0 +1,42 @@
+package lv9;
+
+import java.util.Scanner;
+
+public class No9020 {
+	
+	static boolean PNumber(int num) {
+		int rnum = (int)Math.sqrt(num);
+		if(num==1) {
+			return false;
+		}
+		for(int j=2;j<=rnum;j++) {
+			if(num%j==0)
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for(int i=0;i<t;i++) {
+			int n = sc.nextInt();
+			int half = n/2;
+			int a = half-1, b = half+1;
+			
+			if(PNumber(half)) {
+				System.out.println(half+" "+half);
+			}
+			else {
+				while((PNumber(a) && PNumber(b)) != true) {
+					a--;
+					b++;
+				}
+				System.out.println(a+" "+b);
+			}
+		}
+	}
+
+}
